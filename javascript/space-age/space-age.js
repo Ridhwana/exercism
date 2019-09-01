10618c6b0670cd5464f34c0d7c826047566b2177
Ridhwana <ridhwana.khan16@gmail.com>
2019-09-01 19:49:05 +0200

    refactor: improvement

--- javascript/space-age/space-age.js
+++ javascript/space-age/space-age.js
@@ -1,21 +1,18 @@
-//
-// This is only a SKELETON file for the 'Space Age' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
-
 export const age = (planet, age) => {
-  const seconds = 31557600;
-  const calc = age/(seconds*ORBITAL_PERIOD[planet]);
-  return Math.round(calc * 100) / 100;
+  return roundtoDecimals(age/(EARTH_YEAR_SECONDS * ORBITAL_PERIOD[planet]));
 };
 
-export const ORBITAL_PERIOD = {
+const roundtoDecimals = val => Math.round(val * 100) / 100;
+
+const EARTH_YEAR_SECONDS = 31557600;
+
+const ORBITAL_PERIOD = {
   mercury: 0.2408467,
   venus: 0.61519726,
   earth: 1.0,
   mars: 1.8808158,
   jupiter: 11.862615,
-  saturn:  29.447498,
-  uranus:  84.016846,
-  neptune:  164.79132
+  saturn: 29.447498,
+  uranus: 84.016846,
+  neptune: 164.79132
 };
