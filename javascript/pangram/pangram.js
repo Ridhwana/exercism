f76624b8b3f35358bbb7698a12654c217d5c9ea0
Ridhwana <ridhwana.khan16@gmail.com>
2019-09-01 20:53:24 +0200

    refactor: add a second version

--- javascript/pangram/pangram.js
+++ javascript/pangram/pangram.js
@@ -1,12 +1,14 @@
 export const isPangram = (sentence) => {
   const arr = ALPHABET.split('');
-  for(let char of arr) {
-    if(!sentence.toLowerCase().includes(char)) {
-      return false;
-    }
-  };
 
-  return true;
+  // for(let char of arr) {
+  //   if(!sentence.toLowerCase().includes(char)) {
+  //     return false;
+  //   }
+  // };
+  // return true;
+
+  return arr.every(char => sentence.toLowerCase().includes(char))
 };
 
 
